f80c3a4a33ba5c79263ad404c8b5bd8f9d35672c
fragtastic <fragtastic@users.noreply.github.com>
2021-03-01 15:44:07 -0800

    Use requirements.txt during setup.py automagically

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open('requirements.txt', 'rt', encoding="utf-8") as f:
+    install_requires = f.read().splitlines()
+
 setuptools.setup(
     name="sentinelone-api-python-fragtastic",
     version="0.0.1",
@@ -13,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/fragtastic/sentinelone-api-python",
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
